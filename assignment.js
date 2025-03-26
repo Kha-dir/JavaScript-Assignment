0dae6ebb7ad083982e43efcc4342a3105a683489
Dev_Prime <abdulkadirabdulraheem1@gmail.com>
2025-03-26 16:35:15 -0700

    Create assignment.js

---
+++ assignment.js
@@ -0,0 +1,45 @@
+//JavaScript Assignment Operators
+//Assignment operators assign values to JavaScript variables.
+//The = Operator
+let x = 10;
+console.log(`x is ${x}`);
+
+//The += Operato: Addition Assignment Examples;
+let y = 10;
+y += 5;
+let text = "Dev"; text += " Prime";
+console.log(`addition assignment: ${text}`);
+
+//The -= Operator: Subtraction Assignment Example;
+let z = 10;
+z -= 5;
+console.log(`Subtraction Assignment: ${z}`);
+
+//The *= Operator: Multiplication Assignment Example;
+let b = 10;
+b *= 5;
+console.log(`Multiplication Assignment: ${b}`);
+
+//The **= Operator: Exponentiation Assignment Example.
+let f = 10;
+f **= 5;
+console.log(`Exponentiation Assignment ${f}`);
+
+//The <<= Operator: Left Shift Assignment Example;
+let k = -100;
+k <<= 5;
+console.log(k);
+
+//The >>>= Operator: Unsigned Right Shift Assignment Example;
+let q = -100;
+q >>>= 5;
+console.log(`Unsigned Right Shift Assignment: ${q}`);
+
+//The &= Operator: Bitwise AND Assignment Example;
+let p= 10;
+p&= 5;
+console.log(`Bitwise AND Assignment: ${p}`)
+
+
+
+
